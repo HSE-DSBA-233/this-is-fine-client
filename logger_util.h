1309b2e1428745c63e75091694cf22593bd1f648
Нихад Гурбанов <nihadgurbanov@Nihads-MacBook-Pro.local>
2024-06-16 04:38:16 +0300

    MEGAMAJOR UPDATE

--- logger_util.h
+++ logger_util.h
@@ -1,12 +1,20 @@
 #ifndef LOGGER_UTIL_H
 #define LOGGER_UTIL_H
 
-#include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
+#include <QCoreApplication>
+#include <QDir>
 
 inline std::shared_ptr<spdlog::logger> getLogger() {
-    static std::shared_ptr<spdlog::logger> logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
-    return logger;
+  QDir logDir(QCoreApplication::applicationDirPath() + "/logs");
+  if (!logDir.exists()) {
+    logDir.mkpath(".");
+  }
+  QString logFilePath = logDir.filePath("basic-log.txt");
+  static std::shared_ptr<spdlog::logger> logger =
+      spdlog::basic_logger_mt("basic_logger", logFilePath.toStdString());
+  return logger;
 }
 
 #endif // LOGGER_UTIL_H
--- mainwindow.h
+++ mainwindow.h
@@ -1,70 +1,88 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QStringList>
+#include "chatcreatewindow.h"
+#include "string"
 #include <QListWidget>
+#include <QMainWindow>
+#include <QString>
+#include <QStringList>
 #include <QVBoxLayout>
 #include <QWidget>
-#include <QString>
-#include "chatcreatewindow.h"
 
 class chatsettingswindow;
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
-    Ui::MainWindow* getUi() const { return ui; }
+  Ui::MainWindow *getUi() const { return ui; }
+
+  std::string dir_path();
 
 signals:
-    void passChatSettings(const QString &title, const QString &prompt, const QString &model, const QString &rag);
+  void passChatSettings(const QString &title, const QString &prompt,
+                        const QString &model, const QString &rag);
 
 private slots:
-    void animateButtonPress();
-    void animateButtonRelease();
+  void animateButtonPress();
+  void animateButtonRelease();
 
-    void on_sendMessageButton_clicked();
+  void on_sendMessageButton_clicked();
 
-    void addShadow(QWidget *widget, int blur, int offset);
+  void addShadow(QWidget *widget, int blur, int offset);
 
-    void on_createChatButton_clicked();
+  void on_createChatButton_clicked();
 
-    void on_homeButton_clicked();
+  void on_homeButton_clicked();
 
-    void addMessage(bool isUser, const QString &message);
+  void addMessage(bool isUser, const QString &message);
 
-    void on_chatButton_clicked();
+  void on_chatButton_clicked();
 
-    void handleCreateChat(const QString &title, const QString &prompt, const QString &model, const QString &rag);
+  void handleCreateChat(const QString &title, const QString &prompt,
+                        const QString &model, const QString &rag);
 
-    void handleUpdateChat(const QString &title, const QString &prompt, const QString &model, const QString &rag);
+  void handleUpdateChat(const QString &title, const QString &prompt,
+                        const QString &model, const QString &rag);
 
-    void on_chatslistWidget_itemClicked(QListWidgetItem *item);
+  void on_chatslistWidget_itemClicked(QListWidgetItem *item);
 
-    void chatDeleteClicked();
+  void chatDeleteClicked();
 
-    void on_settingsButton_clicked();
+  void on_settingsButton_clicked();
 
-    void on_clearChatButton_clicked();
+  void on_clearChatButton_clicked();
 
-    void on_settingsChatButton_clicked();
+  void on_settingsChatButton_clicked();
 
-    void on_chatRagButton_clicked();
+  void on_chatRagButton_clicked();
 
+  void saveWindowState();
+
+  void on_chatInput2Widget_returnPressed();
 
 private:
-    Ui::MainWindow *ui;
-    ChatCreateWindow *chatCreateWindow = new ChatCreateWindow();
-    chatsettingswindow *chatSettingsWindow;
+  Ui::MainWindow *ui;
+  ChatCreateWindow *chatCreateWindow = new ChatCreateWindow();
+  chatsettingswindow *chatSettingsWindow;
+  QString typingMessageHtml = "<div style='margin: 10px; padding: "
+                              "15px;'><b><i>loading...</i></b></div>";
+
+protected:
+  void closeEvent(QCloseEvent *event) override {
+    saveWindowState();
+    QMainWindow::closeEvent(event); // Accept the close event
+  }
 };
 
 #endif // MAINWINDOW_H
